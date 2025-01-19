b0343c46cc4a792b3d6e3731622a7d253e60694e
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-01-19 12:23:41 +0100

    slight refactoring and ready to go

--- obj/Debug/net9.0/RAGrocery.AssemblyInfo.cs
+++ obj/Debug/net9.0/RAGrocery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RAGrocery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f16d86f531a116856e37d234a591ed5cf66299")]
 [assembly: System.Reflection.AssemblyProductAttribute("RAGrocery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RAGrocery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
